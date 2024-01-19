775390fde9098add4f1fde411595c46032674838
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2024-01-19 22:51:42 +0200

    completed implementing next button and reset answers

---
+++ React Quize/src/components/Progress.jsx
@@ -0,0 +1,7 @@
+export default function Progress() {
+  return (
+    <header className="progress">
+      <p>Que</p>
+    </header>
+  );
+}
--- React Quize/src/components/Question.jsx
+++ React Quize/src/components/Question.jsx
@@ -3,7 +3,7 @@ export default function Question({ question, answer, dispatch }) {
   return (
     <div>
       <h4>{question.question}</h4>
-      <Option question={question} dispatch={dispatch} answer={dispatch} />
+      <Option question={question} dispatch={dispatch} answer={answer} />
     </div>
   );
 }
